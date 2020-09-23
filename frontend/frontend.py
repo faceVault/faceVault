c78f6559dd0125c051111feb2b4ac103e923593e
Orlando Kenny <kenny@cs.fsu.edu>
2020-09-23 16:09:16 -0400

    add frontend template

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Frontend'
+  return render_template('hello.html')
 
 if __name__ == '__main__':
   app.run(host="localhost", port=5000, debug=True)

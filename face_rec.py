4762ad5a7742522de2f6e12449f0adc546a032e3
Seth <sethpolen@Seths-MacBook-Pro-2.local>
2020-10-29 14:59:33 -0400

    added face_rec file and known & unknown directories

--- face_rec.py
+++ face_rec.py
@@ -21,6 +21,35 @@ known_names = []   # to match with known names
 for name in os.listdir(KNOWN_FACES_DIR):
 	for filename in os.listdir(f"{KNOWN_FACES_DIR}/{name}"):
 		image = face_recognition.load_image_file(f"{KNOWN_FACES_DIR}/{name}/{filename}")
-		encoding = face_recognition.face_encodings(image)[0]
+		encoding = face_recognition.face_encodings(image)[0]  # only want first face it finds
 		known_faces.append(encoding)
 		known_faces.append(name)
+
+print("processing unknown faces")
+for filename in os.listdir(UNKNOWN_FACES_DIR):
+	print(filename)
+	image = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
+	locations = face_recognition.face_locations(image, model=MODEL)
+	encodings = face_recognition.face_encodings(image, locations)
+	image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+	for face_encoding, face_location in zip(encodings, locations):
+		results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
+		match = None
+		if True in results:
+			match = known_names[results.index(True)]
+			print(f"Match found: {match}")
+			# only need top_left and bottom_right coordinate to draw rectangle
+			top_left = (face_location[3], face_location[0])
+			top_right = (face_location[1], face_location[2])
+			color = [0, 255, 0]
+			cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
+			# smaller rectangle with text
+			top_left = (face_location[3], face_location[2])
+			top_right = (face_location[1], face_location[2]+22)
+			cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+			cv2.putText(image, match, (face_location[3]+10, face_location[2]+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), FONT_THICKNESS)
+
+		cv2.imshow(filename, image)
+		cv2.waitKey(10000)
+		# cv2.destroyWindow(filename)
